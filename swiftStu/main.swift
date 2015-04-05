591fff65e470a4c54a25e93b82837ebd2fe9fc50
liugang <liugang@liugangdeMacBook-Pro.local>
2015-04-05 10:51:39 +0800

    Initial Commit

---
+++ swiftStu/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftStu
+//
+//  Created by liugang on 15/4/5.
+//  Copyright (c) 2015年 liugang. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
